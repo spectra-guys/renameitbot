0dfa10a6162b72082e7ac2a78683cbcc43ebeb8d
spectra-guys <69073757+spectra-guys@users.noreply.github.com>
2020-08-14 23:48:00 +0530

    1

---
+++ README.md
@@ -0,0 +1,22 @@
+Bot By BG_OFFICIAL
+---
+
+An Open Source Rename Telegram RoBot
+
+USE the latest repo with Permanent Thumbnail support >> [RENAMEBOT](https://github.com/prgofficial/RenameBot-PermTB)
+
+**My Feature**:
+
+👉 Rename any Telegram Files with Custom Thumbnail Support
+
+### Installation
+
+#### The Easy Way
+
+**Watch this Video for Creating your own Bot** - 👉 https://youtu.be/yYHrSnuYXpA
+
+### You can also tap the Deploy To Heroku button below to deploy straight to Heroku!
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://www.heroku.com/deploy?template=https://github.com/prgofficial/RenameBot)
+
+**upgrade** your subscription for [@AnyDLBot](https://telegram.dog/AnyDLBot) without having to run anything on your own
---
+++ translation.py
@@ -0,0 +1,48 @@
+class Translation(object):
+    START_TEXT = """Hey Buddy,
+This Bot Change Your File Name for You!! 😁
+
+<b>Please send/forward me any Telegram file and reply to that file with /rename moviename .mkv</b>
+
+Please support bot developer @The_Bot_Gram
+
+Our Bot is 100% ad-free.if possible please donate us https://www.buymeacoffee.com/abhinand this would help us alot!!
+/help to discover more thing...
+ """
+    RENAME_403_ERR = "Sorry. You are not permitted to use the bot admin's file"
+    ABS_TEXT = " Don't be selfish"
+    UPGRADE_TEXT = "Simply There is No Way to Upgrade me🤪.Patience is Key to Your Success in Life"
+    DOWNLOAD_START = "trying to download"
+    UPLOAD_START = "trying to upload"
+    RCHD_TG_API_LIMIT = "Got Your Files in {} seconds.\nFound the File Size: {}\nSorry. But Make Sure The File is lesser than this😨😰"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**I Think You Got the File 😍🥰😘. Follow Us @The_Bot_Gram**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Got Your File in our Server in {} seconds.\nSent to you in {} seconds."
+    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved. This image will be used in the File."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "Just Got Your File 🤩👍."
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "You Haven't set any thumbnail 😄"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    HELP_USER = """Hey,Myself I'm a File Rename bot 😇
+    
+Step 1: Send me any Telegram File 😋 or Just forward it here 😜.
+Step 2: Reply to that message with /rename newname .extension. EG : /rename BG(Bot_Gram)_Intro . mp4
+
+🤟🏻 Make sure you don't something illegal🤟🏻
+
+Support Group : @The_Bot_Gram
+👑 @BG_OFFICIAL """
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Just Reply to The File you sent as /rename New_Name . Extension 🙌🏻"
+    ABUSIVE_USERS = "You are restricted to use the bot. If you think I'm wrong just check it with @BG_OFFICIAL"
+    FREE_USER_LIMIT_Q_SZE = """Cannot Process.
+Free users only 1 request per 3 minutes.
+/upgrade or Try 180 seconds later."""
+    IFLONG_FILE_NAME = """File Name limit allowed is {alimit} characters.
+The file name has {num} characters.🥵
+
+<b>Can't get Your filename please we need right name not your life story 🤯</b>
+️ <code>@The_Bot_Gram</code>
+Why do you put this much big name I can't get you 🤫😬😑"""
